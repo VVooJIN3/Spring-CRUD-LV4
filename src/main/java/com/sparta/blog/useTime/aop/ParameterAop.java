f9e23ab3ae902dc5d8402565ad244f2178d988b5
Jo Woojin <dnwls3525@gmail.com>
2023-08-02 20:24:13 +0900

    Spring AOP 적용

--- src/main/java/com/sparta/blog/useTime/aop/ParameterAop.java
+++ src/main/java/com/sparta/blog/useTime/aop/ParameterAop.java
@@ -1,6 +1,5 @@
 package com.sparta.blog.useTime.aop;
 
-import java.lang.reflect.Method;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,53 +8,55 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+
 @Aspect
 @Component
 public class ParameterAop {
 
-  //com/example/aop/controller 패키지 하위 클래스들 전부 적용하겠다고 지점 설정
+    //com/example/aop/controller 패키지 하위 클래스들 전부 적용하겠다고 지점 설정
 
-  @Pointcut("execution(* com.sparta.blog.blog.controller.BlogController.*(..))")
-  private void blog() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.user.controller.UserController.*(..))")
-  private void user() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.reply.controller.ReplyController*(..))")
-  private void reply() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.common.controller.HomeController*(..))")
-  private void Home() {
-  }
-
-
-  //cut() 메서드가 실행 되는 지점 이전에 before() 메서드 실행
-  @Before("blog() || user() || reply() || Home()")
-  public void before(JoinPoint joinPoint) {
-
-    //실행되는 함수 이름을 가져오고 출력
-    MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-    Method method = methodSignature.getMethod();
-    System.out.println(method.getName() + "메서드 실행");
-
-    //메서드에 들어가는 매개변수 배열을 읽어옴
-    Object[] args = joinPoint.getArgs();
-
-    //매개변수 배열의 종류와 값을 출력
-    for (Object obj : args) {
-      System.out.println("type : " + obj.getClass().getSimpleName());
-      System.out.println("value : " + obj);
+    @Pointcut("execution(* com.sparta.blog.blog.controller.BlogController.*(..))")
+    private void blog() {
     }
-  }
 
-  //cut() 메서드가 종료되는 시점에 afterReturn() 메서드 실행
-  //@AfterReturning 어노테이션의 returning 값과 afterReturn 매개변수 obj의 이름이 같아야 함
-  @AfterReturning(value = "blog() || user() || reply() || Home()", returning = "obj")
-  public void afterReturn(JoinPoint joinPoint, Object obj) {
-    System.out.println("return obj");
-    System.out.println(obj);
-  }
+    @Pointcut("execution(* com.sparta.blog.user.controller.UserController.*(..))")
+    private void user() {
+    }
+
+    @Pointcut("execution(* com.sparta.blog.reply.controller.ReplyController*(..))")
+    private void reply() {
+    }
+
+    @Pointcut("execution(* com.sparta.blog.common.controller.HomeController*(..))")
+    private void Home() {
+    }
+
+
+    //cut() 메서드가 실행 되는 지점 이전에 before() 메서드 실행
+    @Before("blog() || user() || reply() || Home()")
+    public void before(JoinPoint joinPoint) {
+
+        //실행되는 함수 이름을 가져오고 출력
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName() + "메서드 실행");
+
+        //메서드에 들어가는 매개변수 배열을 읽어옴
+        Object[] args = joinPoint.getArgs();
+
+        //매개변수 배열의 종류와 값을 출력
+        for (Object obj : args) {
+            System.out.println("type : " + obj.getClass().getSimpleName());
+            System.out.println("value : " + obj);
+        }
+    }
+
+    //cut() 메서드가 종료되는 시점에 afterReturn() 메서드 실행
+    //@AfterReturning 어노테이션의 returning 값과 afterReturn 매개변수 obj의 이름이 같아야 함
+    @AfterReturning(value = "blog() || user() || reply() || Home()", returning = "obj")
+    public void afterReturn(JoinPoint joinPoint, Object obj) {
+        System.out.println("return obj");
+        System.out.println(obj);
+    }
 }
--- src/main/java/com/sparta/blog/useTime/aop/UseTimeAop.java
+++ src/main/java/com/sparta/blog/useTime/aop/UseTimeAop.java
@@ -19,64 +19,64 @@ import org.springframework.stereotype.Component;
 @Component
 public class UseTimeAop {
 
-  private final ApiUseTimeRepository apiUseTimeRepository;
+    private final ApiUseTimeRepository apiUseTimeRepository;
 
-  public UseTimeAop(ApiUseTimeRepository apiUseTimeRepository) {
-    this.apiUseTimeRepository = apiUseTimeRepository;
-  }
-
-  @Pointcut("execution(* com.sparta.blog.blog.controller.BlogController.*(..))")
-  private void blog() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.user.controller.UserController.*(..))")
-  private void user() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.reply.controller.ReplyController*(..))")
-  private void reply() {
-  }
-
-  @Pointcut("execution(* com.sparta.blog.common.controller.HomeController*(..))")
-  private void Home() {
-  }
-
-  @Around("blog() || user() || reply() || Home()")
-  public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-    // 측정 시작 시간
-    long startTime = System.currentTimeMillis();
-
-    try {
-      // 핵심기능 수행
-      Object output = joinPoint.proceed();
-      return output;
-    } finally {
-      // 측정 종료 시간
-      long endTime = System.currentTimeMillis();
-      // 수행시간 = 종료 시간 - 시작 시간
-      long runTime = endTime - startTime;
-
-      // 로그인 회원이 없는 경우, 수행시간 기록하지 않음
-      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-      if (auth != null && auth.getPrincipal().getClass() == UserDetailsImpl.class) {
-        // 로그인 회원 정보
-        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
-        User loginUser = userDetails.getUser();
-
-        // API 사용시간 및 DB 에 기록
-        ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser).orElse(null);
-        if (apiUseTime == null) {
-          // 로그인 회원의 기록이 없으면
-          apiUseTime = new ApiUseTime(loginUser, runTime);
-        } else {
-          // 로그인 회원의 기록이 이미 있으면
-          apiUseTime.addUseTime(runTime);
-        }
-
-        log.info("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: "
-            + apiUseTime.getTotalTime() + " ms");
-        apiUseTimeRepository.save(apiUseTime);
-      }
+    public UseTimeAop(ApiUseTimeRepository apiUseTimeRepository) {
+        this.apiUseTimeRepository = apiUseTimeRepository;
     }
-  }
+
+    @Pointcut("execution(* com.sparta.blog.blog.controller.BlogController.*(..))")
+    private void blog() {
+    }
+
+    @Pointcut("execution(* com.sparta.blog.user.controller.UserController.*(..))")
+    private void user() {
+    }
+
+    @Pointcut("execution(* com.sparta.blog.reply.controller.ReplyController*(..))")
+    private void reply() {
+    }
+
+    @Pointcut("execution(* com.sparta.blog.common.controller.HomeController*(..))")
+    private void Home() {
+    }
+
+    @Around("blog() || user() || reply() || Home()")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 측정 시작 시간
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 핵심기능 수행
+            Object output = joinPoint.proceed();
+            return output;
+        } finally {
+            // 측정 종료 시간
+            long endTime = System.currentTimeMillis();
+            // 수행시간 = 종료 시간 - 시작 시간
+            long runTime = endTime - startTime;
+
+            // 로그인 회원이 없는 경우, 수행시간 기록하지 않음
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null && auth.getPrincipal().getClass() == UserDetailsImpl.class) {
+                // 로그인 회원 정보
+                UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+                User loginUser = userDetails.getUser();
+
+                // API 사용시간 및 DB 에 기록
+                ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser).orElse(null);
+                if (apiUseTime == null) {
+                    // 로그인 회원의 기록이 없으면
+                    apiUseTime = new ApiUseTime(loginUser, runTime);
+                } else {
+                    // 로그인 회원의 기록이 이미 있으면
+                    apiUseTime.addUseTime(runTime);
+                }
+
+                log.info("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: "
+                        + apiUseTime.getTotalTime() + " ms");
+                apiUseTimeRepository.save(apiUseTime);
+            }
+        }
+    }
 }
